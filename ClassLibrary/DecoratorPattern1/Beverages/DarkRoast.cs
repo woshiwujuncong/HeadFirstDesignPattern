653cf8728d5c8b4b8674ccea64c1e9e228efa0d9
卟泪诺 <453354968@qq.com>
2024-06-02 18:58:22 +0800

    添加 装饰者模式第一版 。

---
+++ ClassLibrary/DecoratorPattern1/Beverages/DarkRoast.cs
@@ -0,0 +1,15 @@
+﻿namespace DecoratorPattern1.Beverages
+{
+    public class DarkRoast : Beverage
+    {
+        public DarkRoast()
+        {
+            Description = "Most Excellent Dark Roast";
+        }
+
+        public override double Cost()
+        {
+            return 15 + base.Cost();
+        }
+    }
+}
---
+++ ClassLibrary/DecoratorPattern1/Program.cs
@@ -0,0 +1,18 @@
+﻿using DecoratorPattern1.Beverages;
+
+var espresso = new Espresso
+{
+    Milk = true
+};
+
+var darkRoast = new DarkRoast
+{
+    Soy = true,
+    Whip = true
+};
+
+Console.WriteLine($"espresso cost : {espresso.Cost()}");
+
+Console.WriteLine($"dark roast cost : {darkRoast.Cost()}");
+
+Console.ReadLine();
